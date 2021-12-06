4c7ec2132f756cf4b7431b3c32f1b000d8c818b3
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-06 18:25:57 +0700

    Added loop in printer function

--- Introduction to Go Programming/functions/main.go
+++ Introduction to Go Programming/functions/main.go
@@ -2,11 +2,14 @@ package main
 
 import "fmt"
 
-func printer(msg, msg2 string) {
-	fmt.Printf("%s\n", msg)
-	fmt.Printf("%s\n", msg2)
+func printer(msg, msg2 string, repeat int) {
+	for repeat > 0 {
+		fmt.Printf("%s", msg)
+		fmt.Printf("%s\n", msg2)
+		repeat -= 1
+	}
 }
 
 func main() {
-	printer("Hello,", " World!")
+	printer("Hello,", " World!", 5)
 }
