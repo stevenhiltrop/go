9f5f5aeede82b615ec39aac9a9c3ba6e49cf8a21
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-06 18:11:18 +0700

    Added more cases

--- Introduction to Go Programming/switch/main.go
+++ Introduction to Go Programming/switch/main.go
@@ -13,5 +13,10 @@ func main() {
 		os.Exit(1)
 	case n == 0:
 		fmt.Printf("No bytes output")
+	case n != 14:
+		fmt.Printf("Wrong number of characters: %d", n)
+	default:
+		fmt.Printf("OK!")
 	}
+	fmt.Printf("\n")
 }
