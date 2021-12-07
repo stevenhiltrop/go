064ec2638b013dea4828f2cda5b8363195c90c03
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-07 10:48:09 +0700

    Added byte slicing

---
+++ Introduction to Go Programming/byte_slices/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("test.txt")
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	b := make([]byte, 100)
+
+	n, err := f.Read(b)
+}
