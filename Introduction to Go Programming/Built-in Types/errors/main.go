81f2a69c3af266d7ee5f267c5c07c893c1bc4c4c
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-07 11:25:39 +0700

    Added error handling

--- Introduction to Go Programming/Built-in Types/errors/main.go
+++ Introduction to Go Programming/Built-in Types/errors/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func printer(msg string) error {
+	if msg == "" {
+		return fmt.Errorf("No message was parsed")
+	}
+
+	_, err := fmt.Printf("%s\n", msg)
+
+	return err
+}
+
+func main() {
+	if err := printer(""); err != nil {
+		os.Exit(1)
+	}
+}
