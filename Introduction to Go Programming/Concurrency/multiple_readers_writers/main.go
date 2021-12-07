76a8670d5410906f560c0e4afdde1807fb05d4fc
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-07 15:13:48 +0700

    Added reader and writer code with a generator

--- Introduction to Go Programming/Concurrency/multiple_readers_writers/main.go
+++ Introduction to Go Programming/Concurrency/multiple_readers_writers/main.go
@@ -1,5 +1,57 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
+func getPage(url string) (int, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return 0, err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return 0, err
+	}
+
+	return len(body), nil
+}
+
+func worker(urlCh chan string, sizeCh chan string, id int) {
+	for {
+		url := <-urlCh
+		length, err := getPage(url)
+		if err == nil {
+			sizeCh <- fmt.Sprintf("%s has length %d (%d)", url, length, id)
+		} else {
+			sizeCh <- fmt.Sprintf("Error getting %s: %s", url, err)
+		}
+	}
+}
+
+func generator(url string, urlCh chan string) {
+	urlCh <- url
+}
+
+func main() {
+	urlCh := make(chan string)
+	sizeCh := make(chan string)
+
+	for i := 0; i < 10; i++ {
+		go worker(urlCh, sizeCh, i)
+	}
+
+	urls := []string{"http://www.google.com", "http://www.yahoo.com", "http://www.bing.com", "http://www.oreilly.com", "http://bbc.co.uk"}
+
+	for _, url := range urls {
+		go generator(url, urlCh)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		fmt.Printf("%s\n", <-sizeCh)
+	}
 }
