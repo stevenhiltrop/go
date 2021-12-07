a75591e8066fe6735e77f7f3f7e97ede83d0ffab
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-07 12:19:48 +0700

    Added goroutines and channels examples

--- Introduction to Go Programming/Concurrency/goroutines_channels/main.go
+++ Introduction to Go Programming/Concurrency/goroutines_channels/main.go
@@ -1,6 +1,11 @@
 package main
 
-func emit(c chan string) {
+import (
+	"fmt"
+	"math/rand"
+)
+
+func emitString(c chan string) {
 	words := []string{"The", "quick", "brown", "fox"}
 
 	for _, word := range words {
@@ -9,8 +14,48 @@ func emit(c chan string) {
 	close(c)
 }
 
-func main() {
-	wordChannel := make(chan string)
+func emitRandoms(c chan int) {
 
-	go emit(wordChannel)
+	for {
+		c <- rand.Intn(1000)
+	}
+}
+
+func makeID(idChan chan int) {
+	var id int
+	id = 0
+
+	for {
+		idChan <- id
+		id += 1
+	}
+}
+
+func main() {
+	// Strings
+	/* wordChannel := make(chan string)
+
+	go emitString(wordChannel)
+
+	for word := range wordChannel {
+		fmt.Printf("%s ", word)
+	} */
+
+	// Integers
+	/* randoms := make(chan int)
+
+	go emitRandoms(randoms)
+
+	for n := range randoms {
+		fmt.Printf("%d ", n)
+	} */
+
+	idChan := make(chan int)
+
+	go makeID(idChan)
+
+	fmt.Printf("ID #%d\n", <-idChan)
+	fmt.Printf("ID #%d\n", <-idChan)
+	fmt.Printf("ID #%d\n", <-idChan)
+	fmt.Printf("ID #%d\n", <-idChan)
 }
