535f1424d9df64a14189b576d9a932274204b1a5
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-07 10:38:43 +0700

    Added maps code

---
+++ Introduction to Go Programming/maps/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	dayMonths := make(map[string]int)
+	dayMonths["Jan"] = 31
+	dayMonths["Feb"] = 28
+	dayMonths["Mar"] = 31
+	dayMonths["Apr"] = 30
+	dayMonths["May"] = 31
+	dayMonths["Jun"] = 30
+	dayMonths["Aug"] = 31
+	dayMonths["Sep"] = 30
+	dayMonths["Oct"] = 31
+	dayMonths["Nov"] = 30
+	dayMonths["Dec"] = 31
+
+	fmt.Printf("Days in February: %d\n", dayMonths["Feb"])
+}
