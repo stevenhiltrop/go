a122449b99825016aa864997d0c82ebf03f98d2a
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-07 17:37:10 +0700

    Added shuffler

---
+++ Introduction to Go Programming/User-defined Types/packages_and_initialization/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"shuffler"
+)
+
+type stringSlice []string
+type weightString struct {
+	weight int
+	s      string
+}
+
+func (is stringSlice) Len() int {
+	return len(is)
+}
+
+func (is stringSlice) Swap(i, j int) {
+	is[i], is[j] = is[j], is[i]
+}
+
+func main() {
+	i := stringSlice{weightString{100, "Hello"}, weightString{200, "World!"}, weightString{10, "Goodbye"}}
+	shuffler.Shuffle(i)
+	fmt.Printf("%v", i)
+	fmt.Printf("Loop: %d", shuffler.GetCount())
+}
