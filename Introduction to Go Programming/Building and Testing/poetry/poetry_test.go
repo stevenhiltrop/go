04ec64a8e70b1cec746b3a763f6422a5146ee805
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-08 09:58:18 +0700

    Added NumStanzas unit test

--- Introduction to Go Programming/Building and Testing/poetry/poetry_test.go
+++ Introduction to Go Programming/Building and Testing/poetry/poetry_test.go
@@ -1,1 +1,22 @@
 package poetry
+
+import "testing"
+
+func TestNumStanzas(t *testing.T) {
+	p := Poem{}
+	if p.NumStanzas() != 0 {
+		t.Fatalf("Empty poem is not empty!")
+	}
+
+	p = Poem{{
+		"And from my pillow, looking forth by light",
+		"Of moon or favouring stars, I could behold",
+		"The antechapel where the statue stood",
+		"Of Newton with his prism and silent face",
+		"The marble index of a mind for ever",
+		"Voyagin through strange seas of Thought, alone."}}
+
+	if p.NumStanzas() != 6 {
+		t.Fatalf("Unexpected stanza count %d", p.NumStanzas())
+	}
+}
