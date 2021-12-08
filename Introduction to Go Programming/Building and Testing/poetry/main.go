e13e99be56719fc7fba2e7c27c2f82a2714bf0df
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-08 09:32:04 +0700

    Added poem

---
+++ Introduction to Go Programming/Building and Testing/poetry/main.go
@@ -0,0 +1,25 @@
+package poetry
+
+type Line string
+type Stanza []Line
+type Poem []Stanza
+
+func NewPoem() Poem {
+	return nil
+}
+
+func (p Poem) Stats() (numVowels, numConsonants int) {
+	for _, s := range p {
+		for _, l := range s {
+			for _, r := range l {
+				switch r {
+				case 'a', 'e', 'i', 'o', 'u':
+					numVowels += 1
+				default:
+					numConsonants += 1
+				}
+			}
+		}
+	}
+	return
+}
