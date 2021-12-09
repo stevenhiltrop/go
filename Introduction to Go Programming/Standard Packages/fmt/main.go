3ea53da8efff772f2f8f10263fb50c801efd593f
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-09 08:27:35 +0700

    Added fmt print types

--- Introduction to Go Programming/Standard Packages/fmt/main.go
+++ Introduction to Go Programming/Standard Packages/fmt/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	p := []string{
@@ -11,5 +13,13 @@ func main() {
 		"The marble index of a mind for ever",
 		"Voyagin through strange seas of Thought, alone."}
 
-	fmt.Printf("Type: %T\nString: %s", p[0], p)
+	fmt.Printf("Type: %T\n", p[0])
+
+	result := ""
+
+	for _, s := range p {
+		result += fmt.Sprintf("%s\n", s)
+	}
+
+	fmt.Printf(result)
 }
