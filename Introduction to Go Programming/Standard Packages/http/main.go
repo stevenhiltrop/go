61202968e7b20f989c4dddd2a40d1dbefee52c5a
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-09 09:10:30 +0700

    Added exemplary comment

--- Introduction to Go Programming/Standard Packages/http/main.go
+++ Introduction to Go Programming/Standard Packages/http/main.go
@@ -7,11 +7,12 @@ import (
 
 func poemHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	poemName := r.Form["'Name"][0]
+	poemName := r.Form["'name"][0]
 	fmt.Fprintf(w, "Poem %s coming soon\n", poemName)
 }
 
 func main() {
+	// Example: curl http://127.0.0.1:8088/poem\?name=wordsworth
 	http.HandleFunc("/poem", poemHandler)
 	http.ListenAndServe(":8088", nil)
 }
