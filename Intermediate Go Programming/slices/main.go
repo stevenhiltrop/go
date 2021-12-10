a2598ccee8a5721150b56f070a386b05172a4c01
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-10 15:51:18 +0700

    Added code showcasing modification and removal of elemets in a slice

--- Intermediate Go Programming/slices/main.go
+++ Intermediate Go Programming/slices/main.go
@@ -14,4 +14,20 @@ func main() {
 	middle = append(middle, 42)
 	fmt.Printf("%v\n", middle)
 	fmt.Printf("%v\n", nums)
+	fmt.Printf("%d\n", cap(middle))
+
+	middle = append(middle, 43)
+	middle = append(middle, 44)
+	middle = append(middle, 45)
+	middle = append(middle, 46)
+	fmt.Printf("%v\n", middle)
+	fmt.Printf("%v\n", nums)
+
+	nums[1] *= 2
+	fmt.Printf("%v\n", middle)
+	fmt.Printf("%v\n", nums)
+
+	// Remove
+	middle = append(middle[:3], middle[4:]...)
+	fmt.Printf("%v\n", middle)
 }
