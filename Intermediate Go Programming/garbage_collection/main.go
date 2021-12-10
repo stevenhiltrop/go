70ec90b5bcef3089dc92244875141e9586d2c3e4
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-10 15:56:50 +0700

    Added garbage example

--- Intermediate Go Programming/garbage_collection/main.go
+++ Intermediate Go Programming/garbage_collection/main.go
@@ -1,3 +1,33 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func length(s string) int {
+	c := []byte(s)
+	n := len(c)
+	b := make([]byte, n)
+	return len(b)
+}
+
+func main() {
+	s := "Hello, World!"
+	s += s
+	s += s
+	s += s
+
+	start := time.Now()
+	for {
+		if time.Since(start) > time.Second {
+			var r runtime.MemStats
+			runtime.ReadMemStats(&r)
+			fmt.Printf("Heap size %d\n", r.HeapAlloc)
+			fmt.Printf("NumGC %d\n", r.NumGC)
+			start = time.Now()
+		}
+		length(s)
+	}
+}
