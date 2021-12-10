d9279bd7c4f142da1fd9e8bc70636b6b5d5780f4
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-10 16:59:57 +0700

    Moved folders

--- Intermediate Go Programming/methods_interfaces/methods.go
+++ Intermediate Go Programming/methods_interfaces/methods.go
@@ -9,4 +9,11 @@ func main() {
 	ls := yelling.NewLoudString()
 	ls.Change("Hello")
 	fmt.Println(ls.String())
+
+	n := yelling.NewLoudString
+	ls2 := n()
+
+	change := ls2.Change
+	change("Be quiet")
+	fmt.Println(ls2.String())
 }
