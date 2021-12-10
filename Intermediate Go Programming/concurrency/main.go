f626ac2716e1bf963e9967b8442256a236c90fbd
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-10 18:25:11 +0700

    Added Context type and its functions

--- Intermediate Go Programming/concurrency/main.go
+++ Intermediate Go Programming/concurrency/main.go
@@ -1,12 +1,71 @@
 package main
 
-func main() {
-	c := make(chan int)
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+type Context struct {
+	done chan struct{}
+}
+
+type Counter struct {
+	c    chan int
+	done chan struct{}
+	i    int
+}
+
+func NewContext() *Context {
+	ctx := new(Context)
+	ctx.done = make(chan struct{})
+	return ctx
+}
+
+func NewCounter() *Counter {
+	counter := new(Counter)
+	counter.c = make(chan int)
+	counter.done = make(chan struct{})
+
 	go func() {
-		i := 0
 		for {
-			c <- i
-			i += 1
+			select {
+			case counter.c <- counter.i:
+				counter.i += 1
+			case <-counter.done:
+				return
+			}
 		}
 	}()
+
+	return counter
+}
+
+func (c *Context) GetDone() <-chan struct{} {
+	return c.done
+}
+
+func (c *Counter) GetSource() <-chan int {
+	return c.c
+}
+
+func (c *Context) Stop() {
+	close(c.done)
+}
+
+func (c *Counter) Stop() {
+	c.done <- struct{}{}
+}
+
+func main() {
+	c := NewCounter()
+	read := c.GetSource()
+
+	fmt.Printf("%d\n", <-read)
+	fmt.Printf("%d\n", <-read)
+	fmt.Printf("%d\n", <-read)
+	c.Stop()
+
+	fmt.Printf("%d\n", <-read)
 }
