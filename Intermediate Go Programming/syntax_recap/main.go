54829641a1771c93f952d4847172e953bd98f080
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-10 10:24:15 +0700

    Added error handling

--- Intermediate Go Programming/syntax_recap/main.go
+++ Intermediate Go Programming/syntax_recap/main.go
@@ -1,32 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	var (
-		even  int
-		odd   int
-		total int
+		even   int
+		odd    int
+		zeroes int
+		total  int
+		err    error
 	)
 
-	numbers := []int{1, 2, 3, 5, 0, 7, 8, 9, 10}
+	numbers := []int{1, 2, 3, 5, 0, 7, 8, 9, -3, 10}
 
 Abort:
 	for i := 0; i < 10; i++ {
 		for _, n := range numbers {
 			total += 1
 
-			if n == 0 {
+			switch {
+			case n == 0:
+				zeroes += 1
+			case n%2 == 0:
+				even += 1
+			case n%2 == 1:
+				odd += 1
+			default:
 				break Abort
 			}
-
-			if n%2 == 0 {
-				even += 1
-			} else {
-				odd += 1
-			}
+			total += 1
 		}
 	}
 
-	fmt.Printf("Even %d, odd %d, total %d", even, odd, total)
+	if err != nil {
+		fmt.Printf("Error %s\n", err)
+	}
+
+	fmt.Printf("Even %d, odd %d, zeroes %d, total %d", even, odd, zeroes, total)
 }
