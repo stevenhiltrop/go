d4de7885fe47086138f08def8c32e314733e2a5f
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-10 10:48:31 +0700

    Using return variable n as a new delcaration within an if statement

--- Intermediate Go Programming/syntax_recap/var_declarations.go
+++ Intermediate Go Programming/syntax_recap/var_declarations.go
@@ -2,12 +2,15 @@ package main
 
 import "fmt"
 
+func empty(i []int) (n int) {
+	if n := len(i); n > 0 {
+		n = 1
+	}
+	return
+}
+
 func main() {
 	var intSlice []int = []int{3, 1, 4, 1, 5, 9, 2, 6}
-	stringSlice := []string{"The", "sky", "was", "the", "color"}
 
-	_, err := fmt.Printf("intSlice has %d elements\n", len(intSlice))
-	_, err = fmt.Printf("stringSlice has %d elements\n", len(stringSlice))
-
-	fmt.Printf("%s\n", err)
+	fmt.Printf("%d\n", empty(intSlice))
 }
