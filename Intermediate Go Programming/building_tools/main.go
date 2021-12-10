c7dd6693afad0b89433e7363fbe398e294419c00
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-10 12:34:26 +0700

    Added version flag

--- Intermediate Go Programming/building_tools/main.go
+++ Intermediate Go Programming/building_tools/main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"eecummings"
+	"flag"
 	"fmt"
 	"shouter"
 )
 
+var Version = "1.0.0"
+
 func main() {
+	// go run main.go -version
+	version := flag.Bool("version", false, "Get version")
+	flag.Parse()
+	if *version {
+		fmt.Printf("This is version %s\n", Version)
+		return
+	}
 	fmt.Println(shouter.Shout("Hello, World!"))
 	fmt.Println(eecummings.Simplify("Hello, World!"))
 }
