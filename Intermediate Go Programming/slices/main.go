4874c3e56f5432f43dbcb6511af398f0e2a83de0
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-10 15:45:16 +0700

    Added slices code

--- Intermediate Go Programming/slices/main.go
+++ Intermediate Go Programming/slices/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	nums := []int{1, 11, 21, 1211, 111221, 312211}
+
+	middle := nums[1:3]
+	fmt.Printf("%v\n", middle)
+
+	nums[1] *= 2
+	fmt.Printf("%v\n", middle)
+
+	middle = append(middle, 42)
+	fmt.Printf("%v\n", middle)
+	fmt.Printf("%v\n", nums)
+}
