b32d6e591cd15173708f01edbe632482fa1447fa
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-10 10:44:55 +0700

    Changed throwing errors without delcaring variables

--- Intermediate Go Programming/syntax_recap/var_declarations.go
+++ Intermediate Go Programming/syntax_recap/var_declarations.go
@@ -6,15 +6,8 @@ func main() {
 	var intSlice []int = []int{3, 1, 4, 1, 5, 9, 2, 6}
 	stringSlice := []string{"The", "sky", "was", "the", "color"}
 
-	if n := len(intSlice); n > 0 {
-		fmt.Printf("intSlice has %d elemets\n", n)
-	} else {
-		fmt.Printf("intSlice has no elements\n")
-	}
+	_, err := fmt.Printf("intSlice has %d elements\n", len(intSlice))
+	_, err = fmt.Printf("stringSlice has %d elements\n", len(stringSlice))
 
-	if n := len(stringSlice); n > 0 {
-		fmt.Printf("stringSlice has %d elemets\n", n)
-	} else {
-		fmt.Printf("stringSlice has no elements\n")
-	}
+	fmt.Printf("%s\n", err)
 }
