d3e5705d7fe39d62c9695aded20e5030dbb7165f
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-10 18:56:17 +0700

    Added in comments some usefull tools

---
+++ Intermediate Go Programming/lesser_known_tools/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func Printf(s string) {
+	fmt.Println(s)
+}
+
+func main() {
+	/* CLI:
+	go vet main.go
+	/bin/golint lesser_known_tools
+	go list main.go | more
+		go list -f "{{ .Imports }} lesser_known_tools"
+		go list -f "{{ .Deps }} lesser_known_tools"
+		go list -f "{{ .Dir }} lesser_known_tools"
+		go list -f "{{ .GoFiles }} lesser_known_tools"
+	*/
+	Printf("Hello, World! %s")
+
+	return
+
+	fmt.Println("Exit time")
+}
