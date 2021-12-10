38eddfaeea04429a0e9b743c83e44bf7eeab7d8d
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-10 10:11:44 +0700

    Added breaking a labelled loop

--- Intermediate Go Programming/syntax_recap/main.go
+++ Intermediate Go Programming/syntax_recap/main.go
@@ -3,5 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Hello, World!")
+	var (
+		even  int
+		odd   int
+		total int
+	)
+
+	numbers := []int{1, 2, 3, 5, 0, 7, 8, 9, 10}
+
+Abort:
+	for i := 0; i < 10; i++ {
+		for _, n := range numbers {
+			total += 1
+
+			if n == 0 {
+				break Abort
+			}
+
+			if n%2 == 0 {
+				even += 1
+			} else {
+				odd += 1
+			}
+		}
+	}
+
+	fmt.Printf("Even %d, odd %d, total %d", even, odd, total)
 }
