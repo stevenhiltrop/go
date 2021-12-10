31f5cb06efdcde5185d12116c72d57e88b69f439
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-10 16:44:12 +0700

    Added the sync library to simplify garbage collection

--- Intermediate Go Programming/garbage_collection/reduce.go
+++ Intermediate Go Programming/garbage_collection/reduce.go
@@ -4,21 +4,28 @@ import (
 	"fmt"
 	"math/rand"
 	"runtime"
+	"sync"
 	"time"
 )
 
-func makeBuffer() []byte {
+func makeBuffer() interface{} {
 	return make([]byte, rand.Intn(5000000)+5000000)
 }
 
 func main() {
 	pool := make([][]byte, 200)
 
+	var spare sync.Pool
+	spare.New = makeBuffer
+
 	for i := 0; i < 10; i++ {
 		go func(offset int) {
 			for {
-				b := makeBuffer()
+				b := spare.Get().([]byte)
 				j := offset + rand.Intn(20)
+				if pool[j] != nil {
+					spare.Put(pool[j])
+				}
 				pool[j] = b
 				time.Sleep(time.Microsecond * time.Duration(rand.Intn(1000)))
 			}
