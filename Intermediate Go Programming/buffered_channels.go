1bd984af88d21c765ee758d006ef1297ac49c6a6
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-10 11:00:19 +0700

    Added a worker and controller channel

--- Intermediate Go Programming/buffered_channels.go
+++ Intermediate Go Programming/buffered_channels.go
@@ -1,3 +1,37 @@
 package main
 
-func main() {}
+var availble = make(chan Buffer, 10)
+var toController = make(chan Buffer)
+
+type Buffer struct{}
+
+func (b Buffer) process() {}
+func (b Buffer) get()     {}
+
+func worker() {
+	for {
+		var b Buffer
+		select {
+		case b = <-availble:
+		default:
+			b = Buffer{}
+		}
+		b.get()
+		toController <- b
+	}
+}
+
+func controller() {
+	b := <-toController
+	b.process()
+
+	select {
+	case availble <- b:
+	default:
+	}
+}
+
+func main() {
+	go controller()
+	go worker()
+}
