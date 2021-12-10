fff489e9e0169fe0d34655688809159a8288e07d
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-10 10:27:37 +0700

    Changed Abort label to IgnoreNegative and keep loop going on negative values

--- Intermediate Go Programming/syntax_recap/main.go
+++ Intermediate Go Programming/syntax_recap/main.go
@@ -10,32 +10,23 @@ func main() {
 		odd    int
 		zeroes int
 		total  int
-		err    error
 	)
 
 	numbers := []int{1, 2, 3, 5, 0, 7, 8, 9, -3, 10}
 
-Abort:
-	for i := 0; i < 10; i++ {
-		for _, n := range numbers {
-			total += 1
-
-			switch {
-			case n == 0:
-				zeroes += 1
-			case n%2 == 0:
-				even += 1
-			case n%2 == 1:
-				odd += 1
-			default:
-				break Abort
-			}
-			total += 1
+IgnoreNegative:
+	for _, n := range numbers {
+		switch {
+		case n == 0:
+			zeroes += 1
+		case n%2 == 0:
+			even += 1
+		case n%2 == 1:
+			odd += 1
+		default:
+			continue IgnoreNegative
 		}
-	}
-
-	if err != nil {
-		fmt.Printf("Error %s\n", err)
+		total += 1
 	}
 
 	fmt.Printf("Even %d, odd %d, zeroes %d, total %d", even, odd, zeroes, total)
