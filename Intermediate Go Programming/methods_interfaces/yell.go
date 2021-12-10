e36ebd4ea5cbfb618a7d6be47e079348ef090874
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-10 16:47:39 +0700

    Added yelling example

---
+++ Intermediate Go Programming/methods_interfaces/yell.go
@@ -0,0 +1,23 @@
+package yelling
+
+import "strings"
+
+type LoudString struct {
+	s string
+}
+
+func NewLoudString() LoudString {
+	return LoudString{}
+}
+
+func (ls *LoudString) String() string {
+	return ls.s
+}
+
+func (ls *LoudString) Change(s string) {
+	ls.s = strings.ToUpper(s)
+}
+
+func (ls *LoudString) Blank() {
+	ls.s = ""
+}
