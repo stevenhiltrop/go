aba112d42361e6cee16fb6572a8891dd6d251b5d
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-11 10:49:27 +0700

    Added database code

---
+++ Intermediate Go Programming/databases/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	//_ "https://github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./scientists.db")
+	if err != nil {
+		log.Fatalf("Database error:  %s", err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM names")
+	if err != nil {
+		log.Fatalf("Database error:  %s", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var first string
+		var last string
+		err = rows.Scan(&id, &first, &last)
+		if err != nil {
+			log.Fatalf("Database error:  %s", err)
+		}
+	}
+}
