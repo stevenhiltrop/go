d92e97346806d68e1dd4001b404917c36ff53a5e
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-11 11:57:46 +0700

    Added post function and made switch case in api

--- Intermediate Go Programming/json_api/main.go
+++ Intermediate Go Programming/json_api/main.go
@@ -12,6 +12,63 @@ type databaseServer struct {
 	db *sql.DB
 }
 
+func marshal(w http.ResponseWriter, i interface{}) {
+	j, err := json.Marshal(i)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error %s", err), http.StatusInternalServerError)
+		return
+	}
+	w.Write(j)
+}
+
+func (ds *databaseServer) get(w http.ResponseWriter, req *http.Request, last string) {
+	rows, err := ds.db.Query("SELECT * FROM names WHERE last = ?;", last)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error %s", err), http.StatusBadRequest)
+		return
+	}
+	defer rows.Close()
+
+	var result struct {
+		Success bool
+		Id      int
+		First   string
+		Last    string
+		Alive   bool
+	}
+	if rows.Next() {
+		result.Success = true
+		rows.Scan(&result.Id, &result.First, &result.Last, &result.Alive)
+		marshal(w, result)
+	} else {
+		var result struct {
+			Succes bool
+		}
+		marshal(w, result)
+	}
+}
+
+func (ds *databaseServer) post(w http.ResponseWriter, req *http.Request, last string) {
+	if req.Form["first"] == nil {
+		http.Error(w, fmt.Sprintf("Missing first"), http.StatusBadRequest)
+		return
+	}
+
+	first := req.Form["first"][0]
+
+	_, err := ds.db.Exec("INSERT INTO names (first, last, alive) VALUES (?, ?, 1)", first, last)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error %s", err), http.StatusInternalServerError)
+		return
+	}
+
+	var result struct {
+		Success bool
+	}
+	result.Success = true
+	marshal(w, result)
+}
+
 func (ds *databaseServer) api(w http.ResponseWriter, req *http.Request) {
 	req.ParseForm()
 	if req.Form["last"] == nil {
@@ -21,30 +78,13 @@ func (ds *databaseServer) api(w http.ResponseWriter, req *http.Request) {
 
 	last := req.Form["last"][0]
 
-	rows, err := ds.db.Query("SELECT * FROM names WHERE last = ?;", last)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error %s", err), http.StatusBadRequest)
-		return
-	}
-	defer rows.Close()
-
-	if rows.Next() {
-		var result struct {
-			Id    int
-			First string
-			Last  string
-			Alive bool
-		}
-		rows.Scan(&result.Id, &result.First, &result.Last, &result.Alive)
-
-		j, err := json.Marshal(&result)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Error %s", err), http.StatusInternalServerError)
-			return
-		}
-		w.Write(j)
-	} else {
-		fmt.Fprintf(w, "%s not found\n", last)
+	switch {
+	case req.Method == "GET":
+		ds.get(w, req, last)
+	case req.Method == "POST":
+		ds.post(w, req, last)
+	default:
+		http.Error(w, "Bad method", http.StatusMethodNotAllowed)
 	}
 }
 
